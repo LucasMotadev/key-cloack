2f936d6bd751507f7cbdb803586c66035d1f1a7b
Lucas Mota <lucasmota.dev@gmail.com>
2022-10-21 19:10:55 -0400

    refact: verison e namespace

--- src/Providers/KeyCloackServiceProvider.php
+++ src/Providers/KeyCloackServiceProvider.php
@@ -10,16 +10,16 @@ class KeyCloackServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $key = __DIR__.'/../config/keycloak.php';
+        $key = __DIR__ . '/../config/keycloak.php';
         $value = base_path("config/keyCloack.php");
         $this->publishes([$key => $value], 'config');
         $this->mergeConfigFrom($value, 'keycloak');
     }
 
-     public function register()
-     {
-         Auth::extend('keycloak', function ($app, $name, array $config) {
-             return new KeyCloackGuard(Auth::createUserProvider($config['provider']), $app->request);
-         });
-     }
+    public function register()
+    {
+        Auth::extend('keycloak', function ($app, $name, array $config) {
+            return new KeyCloackGuard(Auth::createUserProvider($config['provider']), $app->request);
+        });
+    }
 }
